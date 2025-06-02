73c39903a4539712188fef0deae7ab181d2dff7d
Michael Teske <michael.teske@outlook.com>
2025-06-02 12:01:43 -0500

    add additional ioc ad  seed scripts

--- detect_AD_iocsv1.ps1
+++ detect_AD_iocsv1.ps1
@@ -3,12 +3,13 @@
 $ReportPath = "C:\LabFiles\Detection_Report_Full.csv"
 $Results = @()
 $Now = Get-Date
+$Days = -15
 
 Write-Host "`n[+] Starting IOC detection..."
 
 # AD Recon via LDAP (4662)
 Write-Host "[*] Detecting AD enumeration..."
-Get-WinEvent -FilterHashtable @{LogName='Security'; Id=4662; StartTime=$Now.AddDays(-30)} |
+Get-WinEvent -FilterHashtable @{LogName='Security'; Id=4662; StartTime=$Now.AddDays($Days)} |
 Where-Object { $_.Message -match "Read Property" } |
 ForEach-Object {
     $Results += [PSCustomObject]@{
@@ -20,7 +21,7 @@ ForEach-Object {
 
 # PowerShell Remoting / ScriptBlock (4104)
 Write-Host "[*] Detecting PowerShell Remoting / Invoke-Command..."
-Get-WinEvent -FilterHashtable @{LogName='Microsoft-Windows-PowerShell/Operational'; Id=4104; StartTime=$Now.AddDays(-30)} |
+Get-WinEvent -FilterHashtable @{LogName='Microsoft-Windows-PowerShell/Operational'; Id=4104; StartTime=$Now.AddDays($Days)} |
 Where-Object { $_.Message -match "Invoke-Command|Enter-PSSession|New-PSSession" } |
 ForEach-Object {
     $Results += [PSCustomObject]@{
@@ -32,7 +33,7 @@ ForEach-Object {
 
 # WinRM Lateral Movement (4624)
 Write-Host "[*] Detecting WinRM-based lateral movement..."
-Get-WinEvent -FilterHashtable @{LogName='Security'; Id=4624; StartTime=$Now.AddDays(-30)} |
+Get-WinEvent -FilterHashtable @{LogName='Security'; Id=4624; StartTime=$Now.AddDays($Days)} |
 Where-Object { $_.Message -match "Logon Type:\s+3" -and $_.Message -match "Impersonation Level:\s+(Impersonation|Delegation)" -and $_.Message -match "Source Network Address" } |
 ForEach-Object {
     $Results += [PSCustomObject]@{
@@ -44,7 +45,7 @@ ForEach-Object {
 
 # Scheduled Task Creation (4698)
 Write-Host "[*] Detecting suspicious scheduled task creation..."
-Get-WinEvent -FilterHashtable @{LogName='Security'; Id=4698; StartTime=$Now.AddDays(-30)} |
+Get-WinEvent -FilterHashtable @{LogName='Security'; Id=4698; StartTime=$Now.AddDays($Days)} |
 ForEach-Object {
     $Results += [PSCustomObject]@{
         TimeCreated = $_.TimeCreated
@@ -68,7 +69,7 @@ ForEach-Object {
 
 # Service Creation (7045)
 Write-Host "[*] Detecting suspicious service creation..."
-Get-WinEvent -FilterHashtable @{LogName='System'; Id=7045; StartTime=$Now.AddDays(-30)} |
+Get-WinEvent -FilterHashtable @{LogName='System'; Id=7045; StartTime=$Now.AddDays($days)} |
 ForEach-Object {
     $Results += [PSCustomObject]@{
         TimeCreated = $_.TimeCreated
